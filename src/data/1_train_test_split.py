409a5b4b853d9e2be0074e9c4b25a173b2cfb9ec
jrbargin <jeanremib@hotmail.com>
2024-10-22 07:51:10 +0000

    Changes to be committed: 	modified:   .gitignore 	modified:   dvc.lock 	modified:   dvc.yaml 	modified:   requirements.txt 	modified:   src/data/1_train_test_split.py 	modified:   src/models/3_gridsearch_modele.py

--- src/data/1_train_test_split.py
+++ src/data/1_train_test_split.py
@@ -1,13 +1,17 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
+import yaml
 
 os.makedirs('/home/ubuntu/exam_dvc/examen-dvc/data/processed', exist_ok=True)
+
+with open("params.yaml", "r") as f:
+    params = yaml.safe_load(f)
 
 df = pd.read_csv('/home/ubuntu/exam_dvc/examen-dvc/data/raw/raw.csv')
 X = df.drop(['date','silica_concentrate'],axis=1)
 y = df['silica_concentrate']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=params['split']['test_size'],random_state=params['split']['random_state'])
 
 
 X_train.to_csv('/home/ubuntu/exam_dvc/examen-dvc/data/processed/X_train.csv', index=False)
--- src/models/3_gridsearch_modele.py
+++ src/models/3_gridsearch_modele.py
@@ -2,17 +2,18 @@ import pandas as pd
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import GridSearchCV
 import joblib
+import yaml
 
 X_train = pd.read_csv('/home/ubuntu/exam_dvc/examen-dvc/data/normalised/X_train_scaled.csv')
 y_train = pd.read_csv('/home/ubuntu/exam_dvc/examen-dvc/data/processed/y_train.csv')
 
+with open("params.yaml", "r") as f:
+    params = yaml.safe_load(f)
+    
 model = Ridge()
-param_grid = {
-    'alpha': [0.1, 1.0, 10.0],  
-    'solver': ['auto', 'svd', 'cholesky']
-}
+param_grid = {'alpha': params['grid_search']['alpha'],'solver': params['grid_search']['solver']}
 
-grid_search = GridSearchCV(model, param_grid, cv=5, scoring='r2')
+grid_search = GridSearchCV(model, param_grid, cv=params['grid_search']['cv'], scoring=params['grid_search']['scoring'])
 grid_search.fit(X_train, y_train.values.ravel())
 
 joblib.dump(grid_search.best_params_, '/home/ubuntu/exam_dvc/examen-dvc/models/best_params.pkl')
